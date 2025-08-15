4309e1764bcb01dcd4aa83e29c575cb60f9343ce
Szymon Motyka <szymonmotyka@gmail.com>
2025-08-15 23:50:35 +0200

    update base

--- AiAgent.Api/Domain/Database/Entites/BaseEntity.cs
+++ AiAgent.Api/Domain/Database/Entites/BaseEntity.cs
@@ -8,9 +8,6 @@ public class BaseEntity
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; }
-    
-    [BsonElement("key")] // Restored
-    public string Key { get; set; } // Restored
 
     [BsonElement("Created")]
     public DateTime Created { get; set; }
